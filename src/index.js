911ecffe612d2d6654343e6c364c963e1993b00c
Ayush <ayushedu684@gmail.com>
2025-12-02 11:01:04 +0530

    connecting to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import fs from "fs";
+import connectDB from "./db/index.js";
+
+const envPath = fs.existsSync("./.env") ? "./.env" : "./public/temp/.env";
+dotenv.config({
+  path: envPath,
+});
+
+connectDB();
+
+/*
+//this was the first approch i used to connect to the database and start the server
+import express from "express";
+const   app = express();
+
+//this is an IIFE from javascript and it runs as soon as it is defined and declared
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.error("Error connecting to the database:", error);
+        throw error;
+       });
+
+       app.listen(process.env.PORT, () => {});
+       console.log(`Server is running on port ${process.env.PORT}`);
+       
+    } catch (error) {
+        console.error
+        ("Error connecting to the database:", error);
+        throw error;
+    }
+})();*/
